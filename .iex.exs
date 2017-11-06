f31292baa861d71c0977e191a499d559ee52f99f
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-11-07 00:58:05 +0800

    Update .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -3,4 +3,4 @@ alias GrabCikgu.Repo
 alias GrabCikgu.Account.User
 alias GrabCikgu.Account.{Role, Video, Profile, StudentProfile}
 alias GrabCikgu.Tutorial
-alias GrabCikgu.Tutorial.{Tutor}
+alias GrabCikgu.Tutorial.{Tutor, Student, Subject, TeachingSubject}
