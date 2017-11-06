7c78ed576ed26f0e1a5781569af320583bd8f706
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-11-07 06:41:18 +0800

    Subject.all

--- lib/grab_cikgu/tutorial/subject.ex
+++ lib/grab_cikgu/tutorial/subject.ex
@@ -1,8 +1,13 @@
 defmodule GrabCikgu.Tutorial.Subject do
   use GrabCikgu.Web, :model
+  alias GrabCikgu.Repo
 
   schema "subjects" do
     field :name, :string
     field :grade, :string
+  end
+
+  def all do
+    Repo.all(__MODULE__)
   end
 end
